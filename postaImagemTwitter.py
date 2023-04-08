c95bb2efe40093588d6ad24f0f28ad2b58ae0d64
pedra42 <pcbx100@gmail.com>
2023-04-08 16:31:41 -0300

    Adicionando osarquivos

---
+++ postaImagemTwitter.py
@@ -0,0 +1,53 @@
+#importações do Selenium
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def PostaImagemNoTwitter(textoDoTweet, LocalDaImagem):
+    #Esta parte configura o navegador
+    options = webdriver.ChromeOptions()
+
+    #Os argumentos definem qual a sessão que será utilizada
+    options.add_argument('--no-sandbox')
+    options.add_argument("--user-data-dir=/home/pedro/.config/google-chrome/Default")
+    options.add_argument("--profile-directory=Default")
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
+
+    #Inicialização do browser
+    browser = webdriver.Chrome(executable_path=r"/usr/bin/chromedriver", options=options)
+
+    #Abre a página do twitter
+    browser.get("https://twitter.com/home")
+
+    #Espera até a área de escrita do tweet estar disponível
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-oyd9sg > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-1dbjc4n.r-184en5c > div > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-bnwqim.r-13qz1uu.r-1g40b8q > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz > label > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > div > div > div > div > div.DraftEditor-editorContainer > div > div > div > div > span")))
+
+    #Clica na área do tweet e manda o texto necessário
+    tweetAreadoTexto = browser.find_element(By.CSS_SELECTOR, "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-oyd9sg > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-1dbjc4n.r-184en5c > div > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-bnwqim.r-13qz1uu.r-1g40b8q > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz > label > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > div > div > div > div > div.DraftEditor-editorContainer > div > div > div > div > span")
+    tweetAreadoTexto.send_keys(textoDoTweet)
+
+    #Seleciona a parte da página que recebe imagem
+    tweetBotaoDaImagem = browser.find_element(By.CSS_SELECTOR, "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-oyd9sg > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div:nth-child(3) > div > div > div:nth-child(1) > input")
+
+    #Envia a localização do arquivo de imagem que se quer fazer o upload e espera um pouco
+    tweetBotaoDaImagem.send_keys(LocalDaImagem)
+    time.sleep(5)
+
+    #Espera até o botão de postagem estar disponível
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-oyd9sg > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div:nth-child(3) > div > div > div:nth-child(2) > div.css-18t94o4.css-1dbjc4n.r-l5o3uw.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-19u6a5r.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr')))
+
+    #Aperta o botão de postaem
+    tweetBotaoDePostagem = browser.find_element(By.CSS_SELECTOR, '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-oyd9sg > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div:nth-child(3) > div > div > div:nth-child(2) > div.css-18t94o4.css-1dbjc4n.r-l5o3uw.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-19u6a5r.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr').click()
+
+    #Espera até a postagem estar completa
+    time.sleep(5)
+
+
+
+
+
+
